fc54e2747508fe0345a077effa2feefbfb2c59cb
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-28 13:22:37 +0200

    Restrictioning on age

--- employee-by-role.py
+++ employee-by-role.py
@@ -3,3 +3,11 @@ while True:
     name= input("Enter employee name, and 'exit' to stop: ")
     if name.lower ()== "exit":
         break
+    while True:
+        age= input ("Provide employee age: ")
+        if age.isdigit ():
+            age=int(age)
+            break
+        else:
+            print ("Enter a valid age: ")
+            
